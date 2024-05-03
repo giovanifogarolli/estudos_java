35dca401e6bf9113943d69b60f204cc4d279c082
Giovani Fogarolli <fogarolligiovani@gmail.com>
2024-05-02 22:53:29 -0300

    Adicionado Kaprekar constant

---
+++ src/main/java/com/individual/projetos/Iniciante/classe/Data.java
@@ -0,0 +1,8 @@
+package com.individual.projetos.Iniciante.classe;
+
+/// Classe define um tipo customizavel, objeto instancia uma classe
+public class Data {
+    int dia;
+    int mes;
+    int ano;
+}
---
+++ src/main/java/com/individual/projetos/Iniciante/classe/DataTeste.java
@@ -0,0 +1,15 @@
+package com.individual.projetos.Iniciante.classe;
+
+public class DataTeste {
+    public static void main(String[] args) {
+        Data d1 = new Data();
+        d1.ano = 2002;
+        d1.dia = 10;
+        d1.mes = 5;
+
+        Data d2 = new Data();
+        d2.ano = 2004;
+        d2.dia = 10;
+        d2.mes = 8;
+    }
+}
